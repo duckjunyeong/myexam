bb5511c68042b7d67a86a7a2ccf14807613a52e3
junyeong <whwnsdud4592@naver.com>
2025-02-01 10:17:11 +0900

    config update

--- back/config/config.js
+++ back/config/config.js
@@ -8,4 +8,11 @@ module.exports = {
     host: "127.0.0.1",
     dialect: "mysql",
   },
+  production: {
+    username: "root",
+    password: process.env.MYSQL_PASSWORD,
+    database: "myexam",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
 };
